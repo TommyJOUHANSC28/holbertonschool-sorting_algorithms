df06c53e2bb33af820866e806c0e010bf64174b5
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-09 15:23:24 +0000

    sort.h

---
+++ sort.h
@@ -0,0 +1,34 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+/*
+* Functions of project
+*/
+void swap_ints(int *a, int *b);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void selection_sort(int *array, size_t size);
+int lomuto_partion(int *array, size_t size, int left, int right);
+void quick_short(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+#endif
